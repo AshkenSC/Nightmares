c7674796c312a7a1bb7f5615cbc4470488be94e8
AshkenSC <393940378@qq.com>
2019-04-14 17:49:35 +0800

    Update 0647, 3/10
    (1) Main camera attributes(position, projection, etc.) set;
    
    (2) CameraFollow script created

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothing = 5f;
+
+    Vector3 offset;     // distance between the camera and the player
+
+    void Start()
+    {
+        // store offset in Start()
+        offset = transform.position - target.position;
+    }
+
+    void FixedUpdate()
+    // in every physic step, use FixedUpdate() to move the camera
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
